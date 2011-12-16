93f690db40cc28db2dac942e850509357c9f0e46
Nicholas Tietz <ntietz@gmail.com>
2011-12-16 13:56:17 -0500

    Added default branch settings for git.

--- config.sh
+++ config.sh
@@ -13,3 +13,7 @@ ln -s ~/Repositories/config/screenrc .screenrc
 rm -f .vimrc
 ln -s ~/Repositories/config/vimrc .vimrc
 
+# set up the default branch for git
+git config --global branch.master.remote origin
+git config --global branch.master.merge refs/heads/master
+

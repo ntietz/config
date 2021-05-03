e90bfc2d2c639c9cf0518fca6263d7a96541a497
Nicholas Tietz-Sokolsky <me@ntietz.com>
2021-05-03 10:01:59 -0400

    Fix gcloud sdk installation

--- install.sh
+++ install.sh
@@ -56,17 +56,7 @@ function install_google_cloud_sdk() {
         return
     fi
 
-    # Create environment variable for correct distribution
-    export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-
-    # Add the Cloud SDK distribution URI as a package source
-    echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-
-    # Import the Google Cloud Platform public key
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
-    sudo apt update
-    sudo apt install google-cloud-sdk
+    sudo snap install google-cloud-sdk --classic
 }
 
 function install_pyenv() {
@@ -115,13 +105,12 @@ function install_git_latest() {
     sudo apt install git
 }
 
-#install_utilities # TODO
-install_docker
-install_docker_compose
-install_asdf
-install_google_cloud_sdk
-install_pyenv
-install_ctags
-install_wtfutil
-install_git_latest
-
+#install_utilities
+#install_docker
+#install_docker_compose
+#install_asdf
+#install_google_cloud_sdk
+#install_pyenv
+#install_ctags
+#install_wtfutil
+#install_git_latest
